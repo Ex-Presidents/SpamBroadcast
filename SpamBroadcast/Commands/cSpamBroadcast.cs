69e541f0582c46c6655143516fbdf8e8cc83f6bc
AtiLion <eliteinfinity1337@gmail.com>
2017-03-17 20:37:25 +0100

    Added code

---
+++ SpamBroadcast/Commands/cSpamBroadcast.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.API;
+using Rocket.Unturned.Player;
+using Rocket.Unturned.Chat;
+using Rocket.Core.Logging;
+using Rocket.API.Extensions;
+
+namespace SpamBroadcast.Commands
+{
+    public class cSpamBroadcast : IRocketCommand
+    {
+        #region Properties
+        public AllowedCaller AllowedCaller { get { return AllowedCaller.Both; } }
+        public string Name { get { return "spambroadcast"; } }
+        public string Help { get { return "Spams a broadcast message as many times as specified"; } }
+        public string Syntax { get { return "/spambroadcast [Times to broadcast] [Message]"; } }
+        public List<string> Aliases { get { return new List<string>() { "sb" }; } }
+        public List<string> Permissions { get { return new List<string>() { "broadcast", "spambroadcast" }; } }
+        #endregion
+
+        public void Execute(IRocketPlayer caller, string[] command)
+        {
+            int repeat;
+
+            if (command.Length < 2)
+            {
+                UnturnedChat.Say(caller, Syntax);
+                return;
+            }
+            if (!int.TryParse(command[0], out repeat))
+            {
+                UnturnedChat.Say(caller, Syntax);
+                return;
+            }
+
+            while (repeat > 0)
+            {
+                UnturnedChat.Say(command.GetParameterString(1));
+                repeat--;
+            }
+        }
+    }
+}
---
+++ SpamBroadcast/SpamBroadcast.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Rocket.Core.Plugins;
+using Rocket.API.Collections;
+using Rocket.Core.Logging;
+
+namespace SpamBroadcast
+{
+    public class SpamBroadcast : RocketPlugin
+    {
+        #region Properties
+        public static SpamBroadcast Instance { get; private set; }
+        #endregion
+
+        protected override void Load()
+        {
+            Instance = this;
+
+            Logger.Log("Spam Broadcast loaded successfully!");
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log("Spam Broadcast unloaded successfully!");
+        }
+
+        #region Functions
+        public override TranslationList DefaultTranslations
+        {
+            get
+            {
+                return new TranslationList()
+                {
+                    // Nothing to see here
+                };
+            }
+        }
+        #endregion
+    }
+}
